9a973c756d2bdc3134709e26f774b14a4b7ffa43
EStanys <edvinasstanys@gmail.com>
2021-05-26 13:46:58 +0300

    people.js added, static path added

--- src/index.js
+++ src/index.js
@@ -5,20 +5,27 @@ const express = require('express'); // isitraukiam express is npm
 const app = express(); // sukuriam express objekta
 const path = require('path'); // kad galetume kelia gaut response
 
+const { people } = require('./js/poeple');
+
+// current paths
+const htmlPath = path.join(__dirname, 'html'); // nurodysim kelia iki index.html failo
 const indexPath = path.join(__dirname, 'html', 'index.html'); // nurodysim kelia iki index.html failo
 const aboutPath = path.join(__dirname, 'html', 'about.html'); // nurodysim kelia iki index.html failo
 console.log(' indexPath', indexPath);
 
-// main route aprasom:
+// routes aprasom:
+// app.get('/', (request, response) => {
+//   response.sendFile(indexPath);
+// });
+// //   response.send('<h1> Hello from Express</h1>');
+// // http://localhost:3000/ gausim index.html faila. responsu issiunciam is serverio index.html faila
 
-app.get('/', (request, response) => {
-  response.sendFile(indexPath);
-});
-//   response.send('<h1> Hello from Express</h1>');
-// http://localhost:3000/ gausim index.html faila. responsu issiunciam is serverio index.html faila
-
-app.get('/about', (req, resp) => resp.sendFile(aboutPath));
+// app.get('/about', (req, resp) => resp.sendFile(aboutPath));
 // i narsykle ivedus adresa su /about gale, gausim tai kas bus response parasyta
+
+// kai turim papke kurions failus norim pasiekti is narsykles pagal pavadinima
+// nustatom static papke
+// app.use(express.static(htmlPath)); // http://localhost:3000/ po / kurio failo pavadinima irasysim i ta faila ir atidarys
 
 // paleidzia serveri ir klausosi httm ir kt requestu nurodytu portu
 app.listen(3000, () => console.log('Server is running')); //portas, kuriam naudosim (klausom porto. Funkcija - ka atliksim kol klausysim porto)
