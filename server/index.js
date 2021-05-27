ac9561850ff3ce3cc8accb6518b3d5f9a5f7fd71
EStanys <edvinasstanys@gmail.com>
2021-05-27 11:06:30 +0300

    add person to people and id count implemented

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // isitraukiam express is npm
 const express = require('express');
 const path = require('path');
--- server/routes/api/peopleApi.js
+++ server/routes/api/peopleApi.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+let personId = 6;
+
 const people = require('../../js/peopleData');
 
 // get all people Endpoint
@@ -25,8 +27,15 @@ router.get('/:id', (req, res) => {
 // Create one people Endpoint
 // gauti duomenis is vartojo formos arba json pavidalu ir sukuri nauja partotoja tarp savo people
 router.post('/', (req, res) => {
-  console.log(' req.body', req.body);
-  res.send(req.body);
+  console.log(' req.body', req.body); //{"name":"James","surname":"Jameson"}
+  const newPerson = {
+    id: (++personId).toString(),
+    name: req.body.name,
+    surname: req.body.surname,
+  };
+  people.push(newPerson);
+  // prideti nauja people objekta
+  res.json(people);
 });
 
 module.exports = router;
