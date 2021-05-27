ebb17e4c1e960b263969e0e62561d8f78c4f8c12
EStanys <edvinasstanys@gmail.com>
2021-05-27 10:47:49 +0300

    added middle ware for handling json and htaml form data

--- server/index.js
+++ server/index.js
@@ -2,52 +2,38 @@
 const express = require('express');
 const path = require('path');
 
-const { people } = require('./js/people');
-
 // sukuriam express app objekta
 const app = express();
 
-// Middle ware - viksmai, vykstantys pries ar po serverio uzklausu/ dazniausiai norim ivykdyti pries musu routus kazkokius
+// Middle ware -- veiksmai vykstantys pries ar po serverio uzklausu
 // logger
 const logger = (req, res, next) => {
-  console.log('logger in action');
   console.log(`${req.protocol}://${req.get('host')}${req.originalUrl} on: ${new Date().toLocaleTimeString()}`);
   next();
 };
 
-//naudoti logger funkcija kaip middle warers
+// naudoti logger funkcija kaip middle ware
 app.use(logger);
 
+// json duomenu atkodavimas/istraukimas is http post requesto
+app.use(express.json());
+// kad galetume dirbti su formos duomenimis body dalyje
+app.use(express.urlencoded({ extended: false }));
+
 // current paths
-const htmlPath = path.join(__dirname, 'html');
+
+const htmlPath = path.join(__dirname, '../client', 'html');
 const indexPath = path.join(__dirname, '../client', 'html', 'index.html');
 const aboutPath = path.join(__dirname, '../client', 'html', 'about.html');
 // console.log(' indexPath', indexPath);
 
-// routes
+// routes for pages
 app.get('/', (req, res) => res.sendFile(indexPath));
 app.get('/about', (req, res) => res.sendFile(aboutPath));
 
-// our api
-app.get('/api/people', (req, res) => {
-  // grazinam json
-  res.json(people);
-});
-// get one people
-app.get('/api/person/:id', (req, res) => {
-  const paramId = req.params.id;
+// api routes
+app.use('/api/people', require('./routes/api/peopleApi'));
 
-  const found = people.find((p) => p.id === paramId);
-
-  if (!found) {
-    res.status(404).json({ errorMsg: `sorry person with id ${paramId} was not found` });
-  }
-
-  // grazinam json
-  res.json(found);
-});
-
-//
 // kai turim papke kurios failus norim pasiekti is narsykles pagal pavadinimas
 // nustatom static papke.
 // app.use(express.static(htmlPath));
---
+++ server/routes/api/peopleApi.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const people = require('../../js/peopleData');
+
+// get all people Endpoint
+router.get('/', (req, res) => {
+  // grazinam json
+  res.json(people);
+});
+// get one people Endpoint
+router.get('/:id', (req, res) => {
+  const paramId = req.params.id;
+
+  const found = people.find((p) => p.id === paramId);
+
+  if (!found) {
+    res.status(404).json({ errorMsg: `sorry person with id ${paramId} was not found` });
+  }
+
+  // grazinam json
+  res.json(found);
+});
+
+// Create one people Endpoint
+// gauti duomenis is vartojo formos arba json pavidalu ir sukuri nauja partotoja tarp savo people
+router.post('/', (req, res) => {
+  console.log(' req.body', req.body);
+  res.send(req.body);
+});
+
+module.exports = router;
